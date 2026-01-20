2f1a8bbbb356f9b1ab2d98b9d21d7ae1097f73eb
gas-liom <alexinardmw@gmail.com>
2026-01-20 10:43:39 +0200

    the initial commit

---
+++ frontend/src/components/AddFruitForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddFruitForm = ({ addFruit }) => {
+  const [fruitName, setFruitName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (fruitName) {
+      addFruit(fruitName);
+      setFruitName('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={fruitName}
+        onChange={(e) => setFruitName(e.target.value)}
+        placeholder="Enter fruit name"
+      />
+      <button type="submit">Add Fruit</button>
+    </form>
+  );
+};
+
+AddFruitForm.propTypes = {
+  addFruit: PropTypes.func.isRequired,
+};
+
+export default AddFruitForm;
